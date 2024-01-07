a3fa3ef98d82b0a783de439dfcf0c9f044ce6780
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-08 00:20:26 +0300

    check user email in service class

--- src/main/java/com/user_management/services/UserService.java
+++ src/main/java/com/user_management/services/UserService.java
@@ -4,14 +4,23 @@ import com.user_management.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
-
     public int registerNewUserServiceMethod(String fname, String lname, String email, String password){
         return userRepository.registerNewUser(fname,lname,email,password);
     }
+    //End of Register New User
+
+    // Check User Email Services Method
+    public List<String> checkUserEmail(String email){
+        return userRepository.checkUserEmail(email);
+    }
+    // End of Check User Email Services Method
+
 }
