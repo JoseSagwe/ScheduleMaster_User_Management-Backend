79d9bdcd53a8f734121b9ad5ced8bb0ddce4086c
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-10 12:04:43 +0300

    Request mapping api

--- src/main/java/com/user_management/rest_controllers/LoginApiController.java
+++ src/main/java/com/user_management/rest_controllers/LoginApiController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/Api/v1")
 public class LoginApiController {
 
     @Autowired
